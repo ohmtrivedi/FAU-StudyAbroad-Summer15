c5d40500ba280741bdc36f409ce1120afe9f2d4a
Ohm Trivedi <ohmtrivedi@gmail.com>
2015-07-15 09:09:48 +0530

    Plot - Blood Pressures (both) vs. Encounter Date for a given patient (Code & Output)

---
+++ SMART/Graphs/SMARTAssign2.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 09 11:57:21 2015
+
+@author: Ohm
+"""
+from xml.etree.ElementTree import parse
+from pandas import DataFrame
+
+doc = parse('generated-data\patient-99912345.fhir-bundle.xml')
+
+#root = doc.getroot()
+
+encounter_dates = []
+
+for encounter in doc.findall('.//{http://hl7.org/fhir}Encounter'):
+    period = encounter.find('{http://hl7.org/fhir}period')
+    start_date = period.find('{http://hl7.org/fhir}start')
+    encounter_dates.append(start_date.get('value'))
+
+print len(encounter_dates)
+print 
+
+systolic_bps = []
+diastolic_bps = []
+
+for observation in doc.findall('.//{http://hl7.org/fhir}Observation'):
+    text_ele = observation.find('{http://hl7.org/fhir}text')
+    div = text_ele.find('{http://www.w3.org/1999/xhtml}div')
+    if div.text.find('Systolic') != -1:
+        value_quant = observation.find('{http://hl7.org/fhir}valueQuantity')
+        value = value_quant.find('{http://hl7.org/fhir}value')
+        systolic_bps.append(value.get('value'))
+    if div.text.find('Diastolic') != -1:
+        value_quant = observation.find('{http://hl7.org/fhir}valueQuantity')
+        value = value_quant.find('{http://hl7.org/fhir}value')
+        diastolic_bps.append(value.get('value'))
+
+#for sys_bp in systolic_bps:
+#    print sys_bp
+
+print len(systolic_bps)
+print
+print len(diastolic_bps)
+#for dias_bp in diastolic_bps:
+#    print dias_bp
+
+enc_dict = {}
+enc_dict['encounter_date'] = encounter_dates
+enc_dict['systolic_bp'] = systolic_bps
+enc_dict['diastolic_bp'] = diastolic_bps
+
+#encounters = DataFrame(enc_dict, columns=['encounter_date', 'diastolic_bp', 'systolic_bp'])
+#encounters = encounters.convert_objects(convert_dates='coerce', convert_numeric=True)
+#encounters.encounter_date = encounters.encounter_date.astype("datetime64")
+#encounters.diastolic_bp = encounters.diastolic_bp.astype(int)
+#print encounters
+
+#encounters.set_index(['encounter_date'],inplace=True)
+#encounters.plot(x_compat=True,marker='o')
+#encounters.groupby([encounters.encounter_date.dt.week, encounters.encounter_date.dt.year]).count().plot(kind='barh')
